ae7e09a6410d309ea66f62bbac2d74365bd88171
Eddie Epstein <eae@us.ibm.com>
2020-06-29 16:00:28 -0400

    Changes so that keys for persistent data are simple strings, not JsonStrings with the extra double quotes; Make set persistent; Add del and showAllState methods

--- servers/simulators/src/main/java/com/ibm/research/reefer/simulator/SimulatorHelper.java
+++ servers/simulators/src/main/java/com/ibm/research/reefer/simulator/SimulatorHelper.java
@@ -1,12 +1,14 @@
 package com.ibm.research.reefer.simulator;
 
-import static com.ibm.research.kar.Kar.actorGetAllState;
-import static com.ibm.research.kar.Kar.actorSetMultipleState;
+import static com.ibm.research.kar.Kar.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.json.Json;
+import javax.json.JsonNumber;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.json.JsonString;
 import javax.json.JsonValue;
 
@@ -28,27 +30,42 @@ public class SimulatorHelper extends BaseActor {
 	public void initState() {
 		if (null == persistentData) {
 			persistentData = new HashMap<String,JsonValue>();
-//			persistentData.put(((JsonValue)Json.createValue("population")).toString(), (JsonValue)Json.createValue(1024));
 		}
 		persistentData.putAll(actorGetAllState(this));
 	}
 
 	@Deactivate
 	public void saveState() {
-//		for (String key : persistentData.keySet()) {
-//			System.out.println("simhelper:saveState");
-//			System.out.println(key+" -> "+persistentData.get(key).toString());
-//		}
-		actorSetMultipleState(this, persistentData);
+		//TODO not necessary as each update is persistent, right?
+//		actorSetMultipleState(this, persistentData);
 	}
 
 	@Remote
 	public JsonValue get(JsonValue key) {
-		return persistentData.get(key.toString());
+		return persistentData.get(((JsonString)key).getString());
 	}
 
 	@Remote
-	public void set(JsonValue key, JsonValue value) {
-		persistentData.put(key.toString(), value);
+	public JsonNumber set(JsonValue key, JsonValue value) {
+		int n = actorSetState(this,((JsonString)key).getString(), value);
+		persistentData.put(((JsonString)key).getString(), value);
+		return Json.createValue(n);
+	}
+
+	@Remote
+	public JsonNumber del(JsonValue key) {
+		int n = actorDeleteState(this, ((JsonString)key).getString());
+		persistentData.remove(((JsonString)key).getString());
+		return Json.createValue(n);
+	}
+
+	@Remote
+	public JsonObject showAllState() {
+		Map<String,JsonValue> tempMap = new HashMap<String,JsonValue>();
+		tempMap.putAll(actorGetAllState(this));
+		
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		tempMap.forEach(builder::add);
+		return builder.build();
 	}
 }

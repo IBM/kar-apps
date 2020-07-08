8680e1c79a51b892d17f86e1d693c302b1e6c946
cwiklik <cwiklikj@gmail.com>
2020-07-08 17:05:36 -0500

    added new controller to handle voyage related rest calls

---
+++ servers/reefer-rest/src/main/java/com/ibm/research/kar/reeferserver/controller/VoyageController.java
@@ -0,0 +1,17 @@
+package com.ibm.research.kar.reeferserver.controller;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.ibm.research.kar.reeferserver.ReeferServerApplication;
+
+@RestController
+@CrossOrigin("*")public class VoyageController {
+
+    
+}

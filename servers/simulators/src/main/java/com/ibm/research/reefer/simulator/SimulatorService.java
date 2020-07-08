65941132a58a8d7f4210dae120597ed81b6ef093
Eddie Epstein <eae@us.ibm.com>
2020-07-08 12:17:48 -0400

    refactor location of in-memory cache from actor to simulator service

--- servers/simulators/src/main/java/com/ibm/research/reefer/simulator/SimulatorService.java
+++ servers/simulators/src/main/java/com/ibm/research/reefer/simulator/SimulatorService.java
@@ -3,8 +3,13 @@ package com.ibm.research.reefer.simulator;
 import static com.ibm.research.kar.Kar.actorCall;
 import static com.ibm.research.kar.Kar.actorRef;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.json.Json;
 import javax.json.JsonNumber;
+import javax.json.JsonObject;
+import javax.json.JsonString;
 import javax.json.JsonValue;
 
 import com.ibm.research.kar.actor.ActorRef;
@@ -12,23 +17,53 @@ import com.ibm.research.kar.actor.exceptions.ActorMethodNotFoundException;
 
 public class SimulatorService {
 
-	public JsonValue setUnitPeriod(JsonValue value) {
-		ActorRef aref = actorRef("simhelper","simservice");
+	private Map<String,JsonValue> persistentData;
+	private ActorRef aref = actorRef("simhelper","simservice");
+
+	// constructor
+	public SimulatorService () {
+		persistentData = new HashMap<String,JsonValue>();
 		try {
-			return actorCall(aref, "set", (JsonValue)Json.createValue("UnitPeriod"), value);
+			persistentData.putAll((JsonObject)actorCall(aref, "getAll"));
 		} catch (ActorMethodNotFoundException e) {
-			System.err.println("setUnitPeriod: actor "+aref.toString()+" not found");
+			System.err.println("SimulatorService: actor "+aref.toString()+" not found");
+			e.printStackTrace();
+		}
+	}
+
+	// local utility to update local cache and persistent state
+	private JsonValue set(JsonValue key, JsonValue value) {
+		persistentData.put(((JsonString)key).getString(), value);
+		return actorCall(aref, "set", key, value);
+	}
+
+	// local utility to retrieve cached value
+	private JsonValue get(JsonValue key) {
+		return persistentData.get(((JsonString)key).getString());
+	}
+
+	public JsonValue setUnitPeriod(JsonValue value) {
+		JsonNumber newval = (((JsonNumber)value).intValue() > 0) ? (JsonNumber)value : (JsonNumber)Json.createValue(0);
+		JsonNumber current = (JsonNumber)this.get((JsonValue)Json.createValue("UnitPeriod"));
+		if (0 == current.intValue() && 0 < newval.intValue()) {
+			//TODO
+			//set any null config values to their default
+			//start Time thread
+		}
+		try {
+			// save new period value
+			return this.set((JsonValue)Json.createValue("UnitPeriod"), (JsonValue)newval);
+		} catch (Exception e) {
 			e.printStackTrace();
 			return 	Json.createValue(-1);
 		}
 	}
 
 	public JsonNumber getUnitPeriod() {
-		ActorRef aref = actorRef("simhelper","simservice");
 		try {
 			return (JsonNumber) actorCall(aref, "get", (JsonValue)Json.createValue("UnitPeriod"));
 		} catch (ActorMethodNotFoundException e) {
-			System.err.println("getUnitPeriod: actor "+aref.toString()+" not found");
+			System.err.println("SimulatorService: actor "+aref.toString()+" not found");
 			e.printStackTrace();
 			return 	Json.createValue(-1);
 		}

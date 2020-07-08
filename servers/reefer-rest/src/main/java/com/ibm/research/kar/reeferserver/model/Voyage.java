4b461f281776efa20e9503b2aa427ec213bca5c1
cwiklik <cwiklikj@gmail.com>
2020-07-08 14:02:39 -0500

    changed the voyage id strategy. Using ship name + sail date for an id

--- servers/reefer-rest/src/main/java/com/ibm/research/kar/reeferserver/model/Voyage.java
+++ servers/reefer-rest/src/main/java/com/ibm/research/kar/reeferserver/model/Voyage.java
@@ -7,42 +7,17 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Voyage {
     private transient final SimpleDateFormat dateFormat 
         = new SimpleDateFormat("MM/dd/yyyy");
-    private static transient AtomicLong idGen = new AtomicLong(1000);
-    private String id;
+   private String id;
     private Route route;
-    /*
-    private String originPort;
-    private String destinationPort;
-    private Ship ship;
-    */
-    // skip this field when serializing into json
+
     private transient Date sailDateObject;
     private String sailDate;
-    /*
-    private int daysAtSea;
-    private int daysAtPort;
-
-    public Voyage(Ship ship, String originPort, String destinationPort, Date sailDateObject, int daysAtSea, int daysAtPort) {
-        this.ship = ship;
-        this.originPort = originPort;
-        this.destinationPort = destinationPort;
-        this.sailDateObject = sailDateObject;
-        this.sailDate = dateFormat.format(v.getSailDate(sailDateObject));
-        this.daysAtSea = daysAtSea;
-        this.daysAtPort = daysAtPort;
-    }
     
-    public Voyage(Route route, String sailDate) {
+    public Voyage(Route route, Date sailDateObject) {
         this.route = route;
-        this.sailDate = sailDate;
-        this.id = String.valueOf(idGen.addAndGet(1));
-    }
-    */
-    public Voyage(Route route, Date sailDate) {
-        this.route = route;
-        this.sailDateObject = sailDate;
+        this.sailDateObject = sailDateObject;
         this.sailDate = dateFormat.format(sailDateObject);
-        this.id = String.valueOf(idGen.addAndGet(1));
+        this.id = String.format("%s-%s",route.getVessel().getName(),this.sailDate).replaceAll("/","-");
     }
     public String getId() {
         return id;
@@ -57,33 +32,6 @@ public class Voyage {
     public String getSailDateAsString() {
         return sailDate;
     }
-    
-    /*
-    public String getOriginPort() {
-        return originPort;
-    }
-
  
-    public String getDestinationPort() {
-        return destinationPort;
-    }
-
- 
-
-    public int getDaysAtSea() {
-        return daysAtSea;
-    }
-
-    public Ship getShip() {
-        return ship;
-    }
-
-
-    public int getDaysAtPort() {
-        return daysAtPort;
-    }
-
- */
-
     
 }

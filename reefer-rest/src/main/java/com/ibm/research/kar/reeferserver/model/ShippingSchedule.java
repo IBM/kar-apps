7e159dab21a7d4b9fa99f1122353d1e39118b84c
cwiklik <cwiklikj@gmail.com>
2020-07-08 19:36:10 -0500

    refactored maven projects to allow for submodules and uber pom

--- reefer-rest/src/main/java/com/ibm/research/kar/reeferserver/model/ShippingSchedule.java
+++ reefer-rest/src/main/java/com/ibm/research/kar/reeferserver/model/ShippingSchedule.java
@@ -1,6 +1,7 @@
 package com.ibm.research.kar.reeferserver.model;
 import java.io.Serializable;
 import java.util.List;
+import com.ibm.research.kar.reefer.common.*;
 
 public class ShippingSchedule implements Serializable {
     private static final long serialVersionUID = 1366354424038297589L;
@@ -8,6 +9,7 @@ public class ShippingSchedule implements Serializable {
 
     public ShippingSchedule() {
         super();
+    //    ReeferAllocator allocator;
     }
     public List<Route> getSchedule() { 
         return schedule; 

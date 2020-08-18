bf9202c7b20660cf4602b79de7edc25933343d14
cwiklik <cwiklikj@gmail.com>
2020-08-18 10:08:16 -0500

    added public int getReeferInventorySize() to ReeferController

--- reefer-rest/src/main/java/com/ibm/research/kar/reeferserver/controller/ReeferController.java
+++ reefer-rest/src/main/java/com/ibm/research/kar/reeferserver/controller/ReeferController.java
@@ -1,14 +1,21 @@
 package com.ibm.research.kar.reeferserver.controller;
 import java.io.IOException;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
 
 import com.ibm.research.kar.reefer.model.Port;
 import com.ibm.research.kar.reefer.model.Reefer;
+import com.ibm.research.kar.reefer.model.Route;
 import com.ibm.research.kar.reeferserver.model.ReeferSupply;
 import com.ibm.research.kar.reeferserver.service.PortService;
 import com.ibm.research.kar.reeferserver.service.ReeferService;
+import com.ibm.research.kar.reeferserver.service.ScheduleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.endpoint.expose.IncludeExcludeEndpointFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,15 +26,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin("*")
 public class ReeferController {
-    @Autowired
+	private int inventorySize=0;
+	@Autowired
     private ReeferService reeferService;
     @Autowired
     private PortService portService;
 	@Autowired
 	private NotificationController webSocket;
+    @Autowired
+    private ScheduleService shipScheduleService;
 
-	public ReeferController() {
-	}
+
+	@PostConstruct
+    public void init() {
+		List<Route> routes = shipScheduleService.getRoutes();
+		Set<String> fleet = new LinkedHashSet<>();
+
+		long fleetMaxCapacity=0;
+		for (Route route: routes) {
+			fleetMaxCapacity += route.getVessel().getMaxCapacity();
+			fleet.add(route.getVessel().getName());
+		}
+		inventorySize = Double.valueOf(fleet.size()*fleetMaxCapacity*1.2).intValue();
+		System.out.println("Inventory Size:::::::"+inventorySize);
+    }
     @PostMapping("/reefers")
 	public   List<Port>  addReefers(@RequestBody ReeferSupply reeferAdd) throws IOException {
 		System.out.println("addReefers() Called - port:"+reeferAdd.getPort()+" howMany:"+reeferAdd.getReeferInventoryCount());
@@ -42,6 +64,11 @@ public class ReeferController {
 		System.out.println("getAllReefers() - Got New Request");
 		
 		return reeferService.getReefers();
+	}
+	@GetMapping("/reefers/inventory/size")
+	public int getReeferInventorySize() {
+		System.out.println("getReeferInventorySize() - Got New Request");
+		return inventorySize;
 	}
 	@GetMapping("/reefers/{port}")
 	public List<Reefer>  getReefers(@RequestParam("port") String port) {

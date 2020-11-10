bc88c7d0291fdf5d4c876d12add789674a7d090f
Eddie Epstein <eae@us.ibm.com>
2020-11-10 09:31:48 -0500

    Optionally inject full rest URL into frontend rather than just the HOST

--- frontend/fe-start.sh
+++ frontend/fe-start.sh
@@ -2,12 +2,14 @@
 
 function modify_index_html {
     sleep 10
-    /bin/sed -i 's/localhost/'"$REST_HOST"'/' /opt/ol/wlp/usr/servers/defaultServer/apps/expanded/reefer-frontend.war/index.html
+    /bin/sed -i 's|http://localhost:9080|'"$REST_URL"'|' /opt/ol/wlp/usr/servers/defaultServer/apps/expanded/reefer-frontend.war/index.html
 }
 
-if [ ! -z $REST_HOST ]; then
-    echo "setting REST_HOST=$REST_HOST"
+if [ ! -z $REST_URL ]; then
+    echo "setting REST_URL=$REST_URL"
     modify_index_html &
+else
+    echo "using default REST_URL=http://localhost:9080"
 fi
 
 /kar/bin/runner
--- scripts/reefer-compose-start.sh
+++ scripts/reefer-compose-start.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 # Script to launch reefer components using docker-compose
+# Frontend GUI may need custom URL to get to reefer-rest
+# Frontend GUI itself will always be at localhost:9088
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 
 cd $SCRIPTDIR
 
-if [ -n "$REST_HOST" ] && [ "$REST_HOST" == "auto" ]; then
-   echo -n "trying to guess host ip ... "
-   REST_HOST=$(host $(hostname) | cut -d' ' -f4)
-   valid=$(echo $REST_HOST | egrep -e "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" > /dev/null)
-   if [ $? != 0 ]; then
-       echo nope
-       echo Please specify actual host ip address to use
-       exit
-   else
-     echo "found $REST_HOST"
-   fi
-fi
+# if [ -n "$REST_HOST" ] && [ "$REST_HOST" == "auto" ]; then
+#    echo -n "trying to guess host ip ... "
+#    REST_HOST=$(host $(hostname) | cut -d' ' -f4)
+#    valid=$(echo $REST_HOST | egrep -e "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" > /dev/null)
+#    if [ $? != 0 ]; then
+#        echo nope
+#        echo Please specify actual host ip address to use
+#        exit
+#    else
+#      echo "found $REST_HOST"
+#    fi
+# fi
 
-if [ -z "$REST_HOST" ]; then
-    resthost=localhost
+if [ -z "$REST_URL" ]; then
+    resturl=http://localhost:9080
 else
-    resthost=$REST_HOST
+    resturl=$REST_URL
 fi
 
 echo Deploying application with docker-compose ...
@@ -32,7 +34,7 @@ docker-compose -f reefer-compose.yaml -p reefer up -d
 echo -n waiting for reefer-rest to be available
 wait=1
 while [ $wait != 0 ]; do
-    response=$(curl -s -H "Content-Type: application/json" -X POST http://localhost:9080/time/currentDate)
+    response=$(curl -s -H "Content-Type: application/json" -X POST ${resturl}/time/currentDate)
     if [ -n "$response" ]; then
 	echo $response | grep -e " " > /dev/null
 	w=$?
@@ -44,4 +46,4 @@ while [ $wait != 0 ]; do
     sleep 2
 done
 
-echo " reefer backend available at http://$resthost:9088"
+echo " reefer GUI available at http://localhost:9088"

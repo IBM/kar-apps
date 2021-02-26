942a9be3ddac2b0be20b96b0e66b5f70a6d1e65f
Eddie Epstein <eae@us.ibm.com>
2021-02-26 12:06:23 -0500

    Add podman play version of reefer app

---
+++ scripts/reefer-play-start.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#
+# Copyright IBM Corporation 2020,2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Script to launch reefer components using podman play
+# usage: reefer-play-start.sh [release]
+
+if [[ -n "$1" ]] && [[ "$1" == "release" ]]; then
+    export IMAGE_PREFIX="quay.io/ibm"
+fi
+if [ -z "$IMAGE_PREFIX" ]; then
+    export IMAGE_PREFIX="localhost:5000/kar"
+fi
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+cd $SCRIPTDIR
+
+# Frontend GUI may need custom URL to get to reefer-rest
+# Frontend GUI itself will always be at localhost:9088
+if [ -z "$REST_URL" ]; then
+    resturl=http://localhost:9080
+else
+    resturl=$REST_URL
+fi
+
+response=$(podman pod list|grep reefer)
+if [ -n "$response" ]; then
+    echo $response | grep Running > /dev/null
+    w=$?
+    if [ $w != 0 ]; then
+	echo "reefer pod already running"
+	exit 1
+    fi
+fi
+
+echo Deploying ${IMAGE_PREFIX} images with podman play
+
+podman play kube reefer-app.yaml -q
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+echo -n waiting for reefer-rest to be available
+wait=1
+while [ $wait != 0 ]; do
+    response=$(curl -s -H "Content-Type: application/json" -X POST ${resturl}/time/currentDate)
+    if [ -n "$response" ]; then
+	echo $response | grep -e " " > /dev/null
+	w=$?
+	if [ $w != 0 ]; then
+	    wait=0
+	fi
+    fi
+    echo -n '.'
+    sleep 2
+done
+
+echo " reefer GUI available at http://localhost:9088"

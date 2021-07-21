f07a0bf2e122cd583dba63df9db7d7bca988f2db
Jaroslaw Cwiklik <cwiklik@jaroslaws-mbp.lan>
2021-07-21 10:18:04 -0400

    implements reefer sharding

---
+++ reefer/actors/src/main/java/com/ibm/research/kar/reefer/actors/AnomalyManagerActor.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright IBM Corporation 2020,2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ibm.research.kar.reefer.actors;
+
+import com.ibm.research.kar.Kar;
+import com.ibm.research.kar.actor.ActorRef;
+import com.ibm.research.kar.actor.annotations.Activate;
+import com.ibm.research.kar.actor.annotations.Actor;
+import com.ibm.research.kar.actor.annotations.Remote;
+import com.ibm.research.kar.reefer.ReeferAppConfig;
+import com.ibm.research.kar.reefer.common.Constants;
+
+import javax.json.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Actor
+public class AnomalyManagerActor extends BaseActor {
+
+    private Map<String, JsonValue> reefersMap = null;
+
+    @Activate
+    public void activate() {
+        Map<String, JsonValue> state = Kar.Actors.State.getAll(this);
+        if (!state.isEmpty()) {
+            if (state.containsKey(Constants.VOYAGE_ORDERS_KEY)) {
+                reefersMap.putAll(state.get(Constants.REEFER_MAP_KEY).asJsonObject());
+                System.out.println("AnomalyManagerActor.activate() - voyage:" + getId() + " restored reefers map - size:" + reefersMap.size());
+            }
+        }
+    }
+
+    @Remote
+    public void depotReefers(JsonObject depotReefers) {
+        System.out.println("AnomalyManagerActor.depotReefers() - " + depotReefers);
+
+        try {
+            JsonArray ja = depotReefers.getJsonArray(Constants.DEPOTS_KEY);
+            int totalCount = depotReefers.getInt(Constants.TOTAL_REEFER_COUNT_KEY);
+            reefersMap = new LinkedHashMap<>(totalCount + 1);
+            for (JsonValue depot : ja) {
+                long t1 = System.currentTimeMillis();
+                int lower = depot.asJsonObject().getInt("reefer-id-lower-bound");
+                int upper = depot.asJsonObject().getInt("reefer-id-upper-bound");
+                Map<String, JsonValue> updateMap = addDepotReefers(depot.asJsonObject().getString(Constants.ANOMALY_TARGET_KEY), lower,
+                        upper);
+                long t2 = System.currentTimeMillis();
+
+                Kar.Actors.State.Submap.set(this, Constants.REEFER_MAP_KEY, updateMap);
+                long t3 = System.currentTimeMillis();
+                reefersMap.putAll(updateMap);
+                updateMap.clear();
+                System.out.println("AnomalyManagerActor.depotReefers() - gen time:" + (t2 - t1) + " save time:" + (t3 - t2) + " map put time:" + (System.currentTimeMillis() - t3) + " reefersMapSize size:" + reefersMap.size() + " update map size:" + updateMap.size());
+            }
+            System.out.println("AnomalyManagerActor.depotReefers() - saved depot reefers");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private Map<String, JsonValue> addDepotReefers(String depot, int lowerRange, int upperRange) {
+        System.out.println("AnomalyManagerActor.addDepotReefers()");
+        Map<String, JsonValue> updateMap = new HashMap<>(upperRange - lowerRange + 1);
+        try {
+            long duration = 0, total = 0;
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            for (int reeferId = lowerRange; reeferId <= upperRange; reeferId++) {
+                long t = System.currentTimeMillis();
+                JsonObject target = reeferTarget(String.valueOf(reeferId), depot, 1, job);
+                duration += (System.currentTimeMillis() - t);
+                long t2 = System.currentTimeMillis();
+                updateMap.put(String.valueOf(reeferId), target);
+                total += (System.currentTimeMillis() - t2);
+            }
+            System.out.println("AnomalyManagerActor.addDepotReefers() ---------- depot:" + depot + " reefer lower range:" + lowerRange + " upper:" + upperRange + " reeferMap size:" + reefersMap.size() + " duration:" + duration + " map update took:" + total);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return updateMap;
+    }
+
+    private JsonObject reeferTarget(String reeferId, String target, int targetType, JsonObjectBuilder job) {
+
+        job.add(Constants.REEFER_ID_KEY, reeferId).
+                add(Constants.TARGET_KEY, target).
+                add(Constants.TARGET_TYPE_KEY, targetType);
+        return job.build();
+    }
+
+    @Remote
+    public void reeferAnomaly(JsonObject message) {
+        try {
+            String reeferId = String.valueOf(message.getInt(Constants.REEFER_ID_KEY));
+            if (reefersMap.containsKey(reeferId)) {
+                JsonObject target = reefersMap.get(reeferId).asJsonObject();
+                System.out.println("AnomalyManagerActor.reeferAnomaly() - ANOMALY :::::::: ID:" + reeferId +
+                        " TARGET >>>>>>>> " + target.getString(Constants.TARGET_KEY) + " TARGET TYPE:" + target.getInt(Constants.TARGET_TYPE_KEY));
+                ActorRef targetActor;
+                switch (target.getInt(Constants.TARGET_TYPE_KEY)) {
+                    case 1:
+                        targetActor = Kar.Actors.ref(ReeferAppConfig.ReeferProvisionerActorName, target.getString(Constants.TARGET_KEY));
+                        Kar.Actors.tell(targetActor, "reeferAnomaly", message);
+                        break;
+                    case 2:
+                        targetActor = Kar.Actors.ref(ReeferAppConfig.OrderActorName, target.getString(Constants.TARGET_KEY));
+                        Kar.Actors.tell(targetActor, "reeferAnomaly", message);
+                        break;
+                }
+            } else {
+                System.out.println("AnomalyManagerActor.reeferAnomaly() - !!!!!!!!!!! reeferId:" + reeferId + " Not Found in inventory");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Remote
+    public void voyageDeparted(JsonObject message) {
+        int count = update(message);
+        System.out.println("AnomalyManagerActor.voyageDeparted() - updated targets for " + count + " reefers");
+    }
+
+    @Remote
+    public void voyageArrived(JsonObject message) {
+        int count = update(message);
+        System.out.println("AnomalyManagerActor.voyageArrived() - updated targets for " + count + " reefers");
+    }
+
+    private int update(JsonObject message) {
+        Map<String, JsonValue> updateMap = new HashMap<>();
+
+        try {
+            JsonArray ja = message.getJsonArray(Constants.TARGETS_KEY);
+            ja.forEach(target -> {
+                String anomalyTarget = target.asJsonObject().getJsonString(Constants.ANOMALY_TARGET_KEY).getString();
+                int targetType = target.asJsonObject().getInt(Constants.ANOMALY_TARGET_TYPE_KEY);
+                String reeferIds = target.asJsonObject().getJsonString(Constants.REEFERS_KEY).getString();
+                String[] rids = reeferIds.split(",");
+                JsonObjectBuilder job = Json.createObjectBuilder();
+                for (String reeferId : rids) {
+                    JsonObject targetObject = reeferTarget(String.valueOf(reeferId), anomalyTarget, targetType, job);
+                    updateMap.put(reeferId, targetObject);
+                    if (reefersMap.containsKey(reeferId)) {
+                        reefersMap.put(reeferId, targetObject);
+                    }
+                }
+                Kar.Actors.State.Submap.set(this, Constants.REEFER_MAP_KEY, updateMap);
+                updateMap.clear();
+            });
+        } catch (Exception e) {
+            System.out.println("AnomalyManagerActor.update() - Error:" + e.getMessage());
+            e.printStackTrace();
+        }
+        return updateMap.size();
+    }
+
+    @Remote
+    public void anomaly(JsonObject message) {
+        System.out.println("AnomalyManager.anomaly()");
+    }
+
+    public static class ReeferLocation {
+        public enum LocationType {
+            DEPOT(1),
+            ORDER(2);
+            public final int type;
+
+            private LocationType(int type) {
+                this.type = type;
+            }
+
+            public int getType() {
+                return type;
+            }
+        }
+
+        ;
+        private int id;
+        private String target;
+        private int targetType;
+
+        public ReeferLocation(int id, String target, int targetType) {
+            this.id = id;
+            this.target = target;
+            this.targetType = targetType;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getTarget() {
+            return target;
+        }
+
+        public int getTargetType() {
+            return targetType;
+        }
+
+        public void setTarget(String target) {
+            this.target = target;
+        }
+
+        public void setTargetType(int targetType) {
+            this.targetType = targetType;
+        }
+    }
+}
---
+++ reefer/actors/src/main/java/com/ibm/research/kar/reefer/actors/DepotManagerActor.java
@@ -0,0 +1,364 @@
+/*
+ * Copyright IBM Corporation 2020,2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ibm.research.kar.reefer.actors;
+
+import com.ibm.research.kar.Kar;
+import com.ibm.research.kar.actor.ActorRef;
+import com.ibm.research.kar.actor.annotations.Activate;
+import com.ibm.research.kar.actor.annotations.Actor;
+import com.ibm.research.kar.actor.annotations.Remote;
+import com.ibm.research.kar.reefer.ReeferAppConfig;
+import com.ibm.research.kar.reefer.common.Constants;
+import com.ibm.research.kar.reefer.common.FleetCapacity;
+import com.ibm.research.kar.reefer.common.json.RouteJsonSerializer;
+import com.ibm.research.kar.reefer.model.Route;
+
+import javax.json.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+@Actor
+public class DepotManagerActor extends BaseActor {
+    private static final Logger logger = Logger.getLogger(OrderManagerActor.class.getName());
+    private List<Depot> depots = new LinkedList<>();
+    private long totalInventorySize = 0;
+
+    @Activate
+    public void activate() {
+        // fetch actor state from Kar storage
+        Map<String, JsonValue> state = Kar.Actors.State.getAll(this);
+        System.out.println("DepotManager.activate() ID:" + getId() + " -------------------------");
+
+        if (state.isEmpty()) {
+            ActorRef scheduleActor = Kar.Actors.ref(ReeferAppConfig.ScheduleManagerActorName, ReeferAppConfig.ScheduleManagerId);
+            JsonValue reply = Kar.Actors.call(scheduleActor, "routes");
+            reply.asJsonArray().stream().forEach(jsonRoute -> {
+                Route route = RouteJsonSerializer.deserialize(jsonRoute.asJsonObject());
+                // the origin and destination must have same reefer inventory size to hash reefers consistently
+                Depot origin = getDepot(route.getOriginPort(), route.getVessel().getMaxCapacity());
+                Depot destination = getDepot(route.getDestinationPort(), route.getVessel().getMaxCapacity());
+                long maxSize = Math.max(origin.size + route.getVessel().getMaxCapacity(), destination.size + route.getVessel().getMaxCapacity());
+                origin.setSize(maxSize);
+                destination.setSize(maxSize);
+            });
+            for (Depot depot : depots) {
+                totalInventorySize += depot.setInventorySize();
+            }
+            int inx = 0;
+            JsonArrayBuilder jab = Json.createArrayBuilder();
+            for (Depot depot : depots) {
+                Shard shard = new Shard(inx, inx + depot.size - 1); // zero based index
+                inx += depot.size;
+                depot.setShard(shard);
+                System.out.print("DepotManager.activate() ............. depot:" + depot.getId() + " " + shard.toString());
+                JsonObjectBuilder job = Json.createObjectBuilder();
+                job.add(Constants.ANOMALY_TARGET_KEY, depot.getId()).
+                        add(Constants.ANOMALY_TARGET_TYPE_KEY, Json.createValue(AnomalyManagerActor.ReeferLocation.LocationType.DEPOT.getType())).
+                        add("reefer-id-lower-bound", depot.getShard().getLowerBound()).
+                        add("reefer-id-upper-bound", depot.getShard().getUpperBound());
+                jab.add(job);
+            }
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add(Constants.TOTAL_REEFER_COUNT_KEY, totalInventorySize).add(Constants.DEPOTS_KEY, jab);
+            ActorRef anomalyManagerActor = Kar.Actors.ref(ReeferAppConfig.AnomalyManagerActorName, ReeferAppConfig.AnomalyManagerId);
+            Kar.Actors.tell(anomalyManagerActor, "depotReefers", job.build());
+
+        } else {
+            Kar.Actors.State.set(this, "TEST", Json.createValue("TEST"));
+        }
+        System.out.println("DepotManager.activate() ID:" + getId() + " ------------------------- returning");
+
+        Kar.Actors.Reminders.schedule(this, "publishReeferMetrics", "AAA", Instant.now().plus(2, ChronoUnit.SECONDS), Duration.ofSeconds(5));
+    }
+
+    @Remote
+    public void publishReeferMetrics() {
+        int booked = 0, currentInventory = 0, inTransitCount = 0, onMaintenance = 0, spoiltReefers = 0;
+        try {
+            for (Depot depot : depots) {
+                JsonValue metrics = Kar.Actors.State.get(depot.depotActor, Constants.REEFER_METRICS_KEY);
+
+                if (metrics != null && metrics != JsonValue.NULL) {
+                    String[] values = ((JsonString) metrics).getString().split(":");
+                    //String[] metrics = ((JsonString)depotMetrics).getString().split(":");
+                    booked += Integer.valueOf(values[0]).intValue();
+                    onMaintenance += Integer.valueOf(values[3]).intValue();
+                }
+
+            }
+            ActorRef scheduleActor = Kar.Actors.ref(ReeferAppConfig.ScheduleManagerActorName, ReeferAppConfig.ScheduleManagerId);
+            JsonValue inTransitMetrics = Kar.Actors.State.get(scheduleActor, Constants.REEFERS_IN_TRANSIT_COUNT_KEY);
+            JsonValue spoiltReefersMetrics = Kar.Actors.State.get(scheduleActor, Constants.TOTAL_SPOILT_KEY);
+            if (inTransitMetrics != null && inTransitMetrics != JsonValue.NULL) {
+                inTransitCount += ((JsonNumber) inTransitMetrics).intValue();
+            }
+            if (spoiltReefersMetrics != null && spoiltReefersMetrics != JsonValue.NULL) {
+                spoiltReefers += ((JsonNumber) spoiltReefersMetrics).intValue();
+            }
+            saveMetrics(booked, inTransitCount, spoiltReefers, onMaintenance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveMetrics(int reefersBooked, int reefersInTransit, int spoilt, int onMaintenance) {
+        String metrics = String.format("%d:%d:%d:%d:%d", reefersBooked, reefersInTransit, spoilt, onMaintenance, totalInventorySize);
+        Kar.Actors.State.set(this, Constants.REEFER_METRICS_KEY, Json.createValue(metrics));
+    }
+
+    private Depot getDepot(String port, int size) {
+        Depot depot = new Depot(port);
+        if (!depots.contains(depot)) {
+            depots.add(depot);
+            return depot;
+        } else {
+            for (Depot existingDepot : depots) {
+                if (existingDepot.equals(depot)) {
+                    return existingDepot;
+                }
+            }
+        }
+        return depot;
+    }
+
+    @Remote
+    public JsonObject depotInventory(JsonValue depotName) {
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        try {
+            String depotId = ((JsonString) depotName).getString();
+            for (Depot depot : depots) {
+                if (depot.getId().equals(depotId)) {
+                    job.add(Constants.TOTAL_REEFER_COUNT_KEY, totalInventorySize).
+                            add(Constants.DEPOT_SIZE_KEY, depot.size).
+                            add("reefer-id-lower-bound", depot.getShard().getLowerBound()).
+                            add("reefer-id-upper-bound", depot.getShard().getUpperBound());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return job.build();
+    }
+
+    @Remote
+    public void newDay(JsonObject jsonDate) {
+        // "broadcast" to all depot new date so that on-maintenance reefers are released
+        for (Depot depot : depots) {
+            // Reefers on maintenance are freed automatically after a configurable number of days passes.
+            Kar.Actors.tell(depot.depotActor, "releaseReefersFromMaintenance", jsonDate);
+        }
+
+    }
+
+    @Remote
+    public void aggregateDepotMetrics() {
+        AggregateDepotMetrics metrics = new AggregateDepotMetrics();
+        depots.forEach(depot -> {
+            Map<String, JsonValue> state = Kar.Actors.State.getAll(depot.getActorRef());
+            if (!state.isEmpty()) {
+                if (state.containsKey(Constants.REEFER_METRICS_KEY)) {
+                    metrics.update(((JsonString) state.get(Constants.REEFER_METRICS_KEY)).getString());
+                }
+            }
+        });
+        Kar.Actors.State.set(Kar.Actors.ref(ReeferAppConfig.DepotManagerActorName, ReeferAppConfig.DepotManagerId),
+                Constants.REEFER_METRICS_KEY, Json.createValue(metrics.toString()));
+    }
+
+    public static class Depot {
+        private final String id;
+        private long size;
+        private final ActorRef depotActor;
+        private Shard shard;
+
+
+        public Depot(String port) {
+            id = makeId(port);
+            //this.size = size;
+            depotActor = Kar.Actors.ref(ReeferAppConfig.ReeferProvisionerActorName, id);
+        }
+
+        public void setSize(long size) {
+            this.size = size;
+        }
+
+        public Shard getShard() {
+            return shard;
+        }
+
+        public void setShard(Shard shard) {
+            this.shard = shard;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public ActorRef getActorRef() {
+            return depotActor;
+        }
+
+        public static String makeId(String port) {
+
+            return port.substring(0, port.indexOf(",")) + Constants.REEFER_DEPOT_SUFFIX;
+        }
+
+        public long setInventorySize() {
+            this.size = FleetCapacity.totalSize(this.size);
+            return this.size;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Depot depot = (Depot) o;
+            return Objects.equals(id, depot.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+    }
+
+    private class Shard {
+        private long lowerBound;
+        private long upperBound;
+
+        public Shard(long lowerBound, long upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+
+        public long getLowerBound() {
+            return lowerBound;
+        }
+
+        public long getUpperBound() {
+            return upperBound;
+        }
+
+        @Override
+        public String toString() {
+            return "Shard{" +
+                    "lowerBound=" + lowerBound +
+                    ", upperBound=" + upperBound +
+                    '}';
+        }
+    }
+
+    private class AggregateDepotMetrics {
+        private int total;
+        private int booked;
+        private int maintenance;
+        private int inTransit;
+        private int spoilt;
+
+        public void update(String metrics) {
+            String[] values = metrics.split(":");
+            booked += Integer.valueOf(values[0].trim());
+            inTransit += Integer.valueOf(values[1].trim());
+            spoilt += Integer.valueOf(values[2].trim());
+            maintenance += Integer.valueOf(values[3].trim());
+            total += Integer.valueOf(values[4].trim());
+        }
+
+        public int incrementTotal(int delta) {
+            this.total += delta;
+            return this.total;
+        }
+
+        public int decrementTotal(int delta) {
+            this.total -= delta;
+            return this.total;
+        }
+
+        public int getTotal() {
+            return this.total;
+        }
+
+        public int incrementBooked(int delta) {
+            this.booked += delta;
+            return this.booked;
+        }
+
+        public int decrementBooked(int delta) {
+            this.booked -= delta;
+            return this.booked;
+        }
+
+        public int getBooked() {
+            return this.booked;
+        }
+
+        public int incrementSpoilt(int delta) {
+            this.spoilt += delta;
+            return this.spoilt;
+        }
+
+        public int decrementSpoilt(int delta) {
+            this.spoilt -= delta;
+            return this.spoilt;
+        }
+
+        public int getSpoilt() {
+            return this.spoilt;
+        }
+
+        public int incrementMaintenance(int delta) {
+            this.maintenance += delta;
+            return this.maintenance;
+        }
+
+        public int decrementMaintenance(int delta) {
+            this.maintenance -= delta;
+            return this.maintenance;
+        }
+
+        public int getOnMaintenance() {
+            return this.maintenance;
+        }
+
+        public int incrementInTransit(int delta) {
+            this.inTransit += delta;
+            return this.inTransit;
+        }
+
+        public int decrementInTransit(int delta) {
+            this.inTransit -= delta;
+            return this.inTransit;
+        }
+
+        public int getInTransit() {
+            return this.inTransit;
+        }
+
+        public String toString() {
+            return String.format("%d:%d:%d:%d:%d", getBooked(), getInTransit(),
+                    getSpoilt(), getOnMaintenance(),
+                    getTotal());
+        }
+    }
+}
